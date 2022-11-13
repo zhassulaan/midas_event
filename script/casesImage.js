59c08038cd0a3870a021c3251df09b1e773b6504
Zhassulan <190103383@stu.sdu.edu.kz>
2022-11-13 23:27:53 +0600

    some changes

--- script/casesImage.js
+++ script/casesImage.js
@@ -25,10 +25,14 @@ function showCasesSlides(n) {
 
 	for (let i = 0; i < text.length; i++) {
 		text[i].style.opacity = "0";
+		text[i].style.height = "0";
 		image[i].style.opacity = "0";
+		image[i].style.height = "0";
 	}
 	text[slideCasesIndex - 1].style.opacity = "1";
+	text[slideCasesIndex - 1].style.height = "max-content";
 	text[slideCasesIndex - 1].style.transition = "all 1.5s ease-in-out";;
 	image[slideCasesIndex - 1].style.opacity = "1";
+	image[slideCasesIndex - 1].style.height = "max-content";
 	image[slideCasesIndex - 1].style.transition = "all 1.5s ease-in-out";;
 }
--- script/navbar.js
+++ script/navbar.js
@@ -1,12 +1,14 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  if (window.scrollY >= 25) {
-    document.getElementById("header").style.backgroundColor = "var(--clr-black)";
-    document.getElementById("header").style.padding = "0.9375rem 0";
-    document.getElementById("header").style.transition = "background-color 0.15s ease-in-out, padding 0.15s ease-in-out";
-  } else {
-    document.getElementById("header").style.backgroundColor = "transparent";
-    document.getElementById("header").style.padding = "1.875rem 0";
-    document.getElementById("header").style.transition = "background-color 0.15s ease-in-out, padding 0.15s ease-in-out";
+  if (window.innerWidth > 768) {
+    if (window.scrollY >= 25) {
+      document.getElementById("header").style.backgroundColor = "var(--clr-black)";
+      document.getElementById("header").style.padding = "0.9375rem 0";
+      document.getElementById("header").style.transition = "background-color 0.15s ease-in-out, padding 0.15s ease-in-out";
+    } else {
+      document.getElementById("header").style.backgroundColor = "transparent";
+      document.getElementById("header").style.padding = "1.875rem 0";
+      document.getElementById("header").style.transition = "background-color 0.15s ease-in-out, padding 0.15s ease-in-out";
+    }
   }
 }
