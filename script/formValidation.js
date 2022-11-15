1c8be8cd3233f3e65c2e0c6f4667d8523b8e2ade
Zhassulan <190103383@stu.sdu.edu.kz>
2022-11-15 17:01:50 +0600

    mask input

--- script/formValidation.js
+++ script/formValidation.js
@@ -1,6 +1,9 @@
 function validatePhoneNumber(str) {
-	var phonePattern = /^\d{11}$/;
-	return phonePattern.test(str);
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === '_')
+			return false;
+	}
+	return true;
 }
 const validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
  
@@ -42,4 +45,45 @@ function validation() {
 		text.textContent = "Заполните все необходимые поля";
 		button.style.backgroundColor = "var(--clr-black)";
 	}
+}
+
+
+Array.prototype.forEach.call(document.body.querySelectorAll("*[data-mask]"), applyDataMask);
+
+function applyDataMask(field) {
+   var mask = field.dataset.mask.split('');
+    
+   // For now, this just strips everything that's not a number
+   function stripMask(maskedData) {
+      function isDigit(char) {
+         return /\d/.test(char);
+      }
+      return maskedData.split('').filter(isDigit);
+   }
+    
+   // Replace `_` characters with characters from `data`
+   function applyMask(data) {
+      return mask.map(function(char) {
+         if (char != '_') return char;
+         if (data.length == 0) return char;
+         return data.shift();
+      }).join('')
+   }
+    
+   function reapplyMask(data) {
+      return applyMask(stripMask(data));
+   }
+    
+   function changed() {   
+      var oldStart = field.selectionStart;
+      var oldEnd = field.selectionEnd;
+        
+      field.value = reapplyMask(field.value);
+        
+    	field.selectionStart = oldStart;
+      field.selectionEnd = oldEnd;
+   }
+    
+   field.addEventListener('click', changed)
+   field.addEventListener('keyup', changed)
 }
